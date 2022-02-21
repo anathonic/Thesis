578b165158ab8b48d097e000d2551ab5b17a3a48
mwojtkowiak <49155326+mwojtkowiak98@users.noreply.github.com>
2022-02-21 19:43:56 +0100

    Map for Contact Page

--- food-application/src/views/website/Contact.vue
+++ food-application/src/views/website/Contact.vue
@@ -7,7 +7,9 @@
       <div class="card border-0 rounded-3 shadow-lg overflow-hidden">
         <div class="card-body p-0">
           <div class="row g-0">
-            <div class="col-sm-6 d-none d-sm-block bg-image"></div>
+            <div class="col-sm-6 d-none d-sm-block bg-image" id="googleMap" style="padding: 20px">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2433.4959360188673!2d16.929336115996158!3d52.41581115169757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47045b458e6d37f9%3A0x630f1e648e79b67!2sCollegium%20Da%20Vinci!5e0!3m2!1spl!2spl!4v1645468286870!5m2!1spl!2spl" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
             <div class="col-sm-6 p-4">
               <div class="text-center">
                 <img src="../../../src/assets/mycollection/png/contact/mail-7.png" class="img-fluid mb-2" alt="Responsive image">
